c5ce00d3f144f4a2f66178c648623dfd693051d6
Salman Qureshi <isalmanqureshi@gmail.com>
2024-03-13 22:25:36 +0530

    implemented bottom tabs and agenda calendar view

--- App.js
+++ App.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import PrayerTimes from './PrayerTimes';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import PrayerTimeScreen from './PrayerTimes';
+import CalendarScreen from './CalendarScreen';
 import SettingsScreen from './SettingsScreen';
 import AboutUsScreen from './AboutUsScreen';
 
+
 const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === 'Calendar') {
+            iconName = 'calendar';
+          } else if (route.name === 'Today') {
+            iconName = 'clock-o';
+          }
+
+          // You can return any component here
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
+      <Tab.Screen name="Today" component={PrayerTimeScreen} />
+      <Tab.Screen name="Calendar" component={CalendarScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="PrayerTimes">
-        <Drawer.Screen name="PrayerTimes" component={PrayerTimes} />
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={TabNavigator} />
         <Drawer.Screen name="Settings" component={SettingsScreen} />
         <Drawer.Screen name="About Us" component={AboutUsScreen} />
       </Drawer.Navigator>
---
+++ CalendarScreen.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+import { Agenda } from 'react-native-calendars';
+
+const CalendarScreen = () => {
+  const [agendaData, setAgendaData] = useState({});
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        'https://raw.githubusercontent.com/hotheadhacker/json-data/main/ramdan-2024.json'
+      );
+      const data = await response.json();
+      const formattedData = formatData(data);
+      setAgendaData(formattedData);
+    } catch (error) {
+      console.error('Error fetching data: ', error);
+    }
+  };
+
+  const formatData = (data) => {
+    const formattedData = {};
+    Object.keys(data.data).forEach((date) => {
+      const formattedDate = formatDate(date);
+      formattedData[formattedDate] = [
+        { day: data.data[date].day, iftar: data.data[date].iftar, sehri: data.data[date].sehri }
+      ];
+    });
+    return formattedData;
+  };
+
+  const formatDate = (dateString) => {
+    // Assuming dateString is in the format "dd-mm-yyyy"
+    const parts = dateString.split('-');
+    return `${parts[2]}-${parts[1]}-${parts[0]}`;
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      <Agenda
+        items={agendaData}
+        renderItem={(item) => {
+          return (
+            <View style={{ padding: 20 }}>
+              <Text>Day: {item.day}</Text>
+              <Text>Iftar: {item.iftar} AM</Text>
+              <Text>Sahar: {item.sehri} PM</Text>
+            </View>
+          );
+        }}
+      />
+    </View>
+  );
+};
+
+export default CalendarScreen;
+
